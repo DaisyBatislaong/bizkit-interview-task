f908c1eb81ff8309ca7e3df238ff0043e2665b8c
DaisyBatislaong <daisyjanebatislaong1124@gmail.com>
2024-05-19 13:20:47 +0800

    [UPDATE] add numpy library to optimize match search

--- phasebook/match.py
+++ phasebook/match.py
@@ -1,8 +1,14 @@
 import time
 from flask import Blueprint
+# from flask import Flask
+import logging
+import numpy as num
 
 from .data.match_data import MATCHES
+# app = Flask(__name__)
 
+# logging.basicConfig(filename='flask.log', level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
 
 bp = Blueprint("match", __name__, url_prefix="/match")
 
@@ -11,17 +17,19 @@ bp = Blueprint("match", __name__, url_prefix="/match")
 def match(match_id):
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
-
     start = time.time()
     msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
     end = time.time()
-
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    fave_numbers_1 = num.array(fave_numbers_1)
+    fave_numbers_2 = num.array(fave_numbers_2)
+    if num.any(num.isin(fave_numbers_2, fave_numbers_1, invert=True)):
+        return False
+    # for number in fave_numbers_2:
+    #     if number not in fave_numbers_1:
+    #         return False
 
     return True
