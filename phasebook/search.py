cc5f69c69efc516f85117cec214473cfcbd0a199
DaisyBatislaong <daisyjanebatislaong1124@gmail.com>
2024-05-20 08:51:32 +0800

    [UPDATE] update logic into 1 user only per entry

--- phasebook/search.py
+++ phasebook/search.py
@@ -1,9 +1,15 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Flask
 
 from .data.search_data import USERS
 
 
 bp = Blueprint("search", __name__, url_prefix="/search")
+
+
+import logging
+app = Flask(__name__)
+logging.basicConfig(filename='flask.log', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @bp.route("")
@@ -24,7 +30,41 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
-
     # Implement search here!
 
-    return USERS
+
+    users_match = []
+    search_order= [key for key, value in args.items()]
+    encountered_ids = []
+    for user in USERS:
+        app.logger.info(encountered_ids)
+
+        if args.get('id') and (args['id'] == user['id']) and user['id'] not in encountered_ids:
+            match_order = search_order.index('id')
+            users_match.append((user, match_order))
+            encountered_ids.append(user['id'])
+
+        # for matching age
+        if args.get('age') and (int(args['age']) - 1 <= user['age'] <= int(args['age'])+1) and user['id'] not in encountered_ids: 
+            match_order = search_order.index('age')
+            users_match.append((user, match_order))
+            encountered_ids.append(user['id'])
+        
+        # For matcing names
+        if args.get('name') and user['id'] not in encountered_ids: 
+            search_name = args.get('name', '').split()
+            user_names = user['name'].split()
+            if all(word in user_names for word in search_name):
+                match_order = search_order.index('name')
+                app.logger.info(match_order)
+                users_match.append((user, match_order))
+                encountered_ids.append(user['id'])
+
+        if args.get('occupation') and any(char in user['occupation'] for char in args['occupation']) and user['id'] not in encountered_ids:
+            match_order = search_order.index('occupation')
+            users_match.append((user, match_order))
+            encountered_ids.append(user['id'])
+
+    sorted_data = sorted(users_match, key=lambda x: x[1])
+    sorted_user_data = [user_data[0] for user_data in sorted_data]
+    return sorted_user_data
